9f5c0dbf8a5cec5f8bb73e2f311c761a7632d4dd
Zhouxinyang <741244407@qq.com>
2017-02-13 10:36:48 +0800

    简单数组排序

--- RequireJS/数组排序/index.js
+++ RequireJS/数组排序/index.js
@@ -1,5 +1,18 @@
+//requirejs
+
 require(["a"],function(arrSort){
+
     var arr = [3,2,5,1,11];
     //arr="ssd";
+    console.log(arrSort(arr));
+});
+
+
+
+
+//Sea.js
+define(function(require){
+    var arr = [3,2,5,1,11];
+    var arrSort = require("a");
     console.log(arrSort(arr));
 });
