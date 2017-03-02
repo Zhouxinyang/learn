aa80b8e9bc49c6093557f6fa2d7ece228b400368
Zhouxinyang <741244407@qq.com>
2017-03-02 20:38:38 +0800

    angular练习

---
+++ angular-test/app/cart.js
@@ -0,0 +1,68 @@
+function cartController($scope){
+	$scope.carts=[
+		{
+			pId:1000,
+			pName:'iphone6s',
+			pnum:3,
+			price:5400,
+		},
+		{
+			pId:1200,
+			pName:'mac',
+			pnum:3,
+			price:15400,
+		},
+		{
+			pId:2000,
+			pName:'iphone7s',
+			pnum:10,
+			price:7400,
+		},
+		{
+			pId:3000,
+			pName:'ipad',
+			pnum:21,
+			price:2400,
+		}
+	];
+
+
+	$scope.sum=function(){
+		var sum=0;
+		angular.forEach($scope.carts,function(value, key){
+			sum+=value.price*value.pnum;
+		});
+
+		return sum;
+
+	}
+
+	$scope.total=function(){
+		var total=0;
+		angular.forEach($scope.carts,function(value, key){
+			total+=parseInt(value.pnum);
+			//console.log(key);
+		});
+
+		return total;
+
+	}
+
+	$scope.del=function(id){
+		//console.log(id);
+		var index=-1;
+		angular.forEach($scope.carts,function(value,key){
+			if(value.pId==id){
+				index=key;
+			}
+		});
+
+		if(index != -1){
+			$scope.carts.splice(index,1);
+		}
+	}
+
+	$scope.destory=function(){
+		$scope.carts={};
+	}
+}
